c3606440dd91c2c420738b3dba2cfba848d68a61
Rock de Vocht <peter@peter.co.nz>
2017-07-14 10:54:32 +1200

    set time to seconds

--- util/sound_splitter.py
+++ util/sound_splitter.py
@@ -6,7 +6,7 @@ import tempfile
 # top_db : number > 0, The threshold (in decibels) below reference to consider as silence
 # frame_length : int > 0, The number of samples per analysis frame
 # hop_length : int > 0, The number of samples between analysis frames
-def _split_sound_file(filename: str, top_db=20, frame_length=4096, hop_length=512, sr=8000):
+def _split_sound_file(filename: str, top_db=20, frame_length=4096, hop_length=512, sr=16000):
     # load the soundfile
     y, sr = librosa.load(filename, mono=True, sr=sr)
     i_vals = librosa.effects.split(y, top_db=top_db, frame_length=frame_length, hop_length=hop_length)
@@ -24,7 +24,7 @@ def split_soundfile_into_many(job_id: str, sound_filename: str, top_db=20, frame
     for interval in intervals:
         filename = os.path.join(tempfile.gettempdir(), job_id + '_' + str(counter) + '.wav')
         librosa.output.write_wav(filename, y[interval[0]:interval[1]], sr)
-        filename_list.append((filename, interval[0]))
+        filename_list.append((filename, float(interval[0]) / float(sr)))
         counter += 1
 
     return filename_list
