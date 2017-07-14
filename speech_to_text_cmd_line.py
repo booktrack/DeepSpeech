420c70ae25e5db90267185b6cd9c4a7c6e8c7ecb
Rock de Vocht <peter@peter.co.nz>
2017-07-14 12:06:39 +1200

    added db to cmd line

--- speech_to_text_cmd_line.py
+++ speech_to_text_cmd_line.py
@@ -14,12 +14,17 @@ logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == '__main__' :
 
-    if len(sys.argv) != 2:
-        print("takes one argument: sound-file to convert to text")
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
+        print("takes two arguments (second parameter optional): /path/to/soundfile.mp3  [noise_db]")
+        print("      e.g.  ./speech_to_text_cmd_line.py /test/file.ogg 30")
     else:
         input_sound_file = sys.argv[1]
+        silence_db = int(conf["DeepSpeech"]["silence_db"])
+        if len(sys.argv) > 2:
+            silence_db = int(sys.argv[2])
+
         temp_name = next(tempfile._get_candidate_names())
 
-        text_output_list = deep_speech_tt(conf["DeepSpeech"], temp_name, input_sound_file, int(conf["DeepSpeech"]["silence_db"]))
+        text_output_list = deep_speech_tt(conf["DeepSpeech"], temp_name, input_sound_file, silence_db)
         for text_out in text_output_list:
             print(text_out)
