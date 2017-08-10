ac2ef2c226409f14886ecb8315a95c916a27cd79
Rock de Vocht <peter@peter.co.nz>
2017-08-10 14:35:01 +1200

    fixed wer calculations

--- calculate_test_wer.py
+++ calculate_test_wer.py
@@ -4,6 +4,7 @@ import os
 from functools import reduce
 import tempfile
 import logging
+import datetime
 
 from util.configuration import get_configuration
 from util.speech_to_text import deep_speech_tt
@@ -27,8 +28,31 @@ def calc_wer(t1: str, t2: str):
         for i in range(0, len(parts_1)):
             if parts_1[i] != parts_2[i]:
                 num_wrong += 1.0
-        return num_wrong / len(parts_1)
-    return -1.0  # can't do sequence
+        return num_wrong / len(parts_1), num_wrong, len(parts_1)
+    else:
+        num_wrong = 0.0
+        i1 = 0
+        i2 = 0
+        while i1 < len(parts_1) and i2 < len(parts_2):
+            if parts_1[i1] != parts_2[i2]:
+                num_wrong += 1.0
+                if i1 + 1 < len(parts_1) and parts_1[i1 + 1] == parts_2[i2]:
+                    i1 += 1
+                elif i2 + 1 < len(parts_2) and parts_1[i1] == parts_2[i2 + 1]:
+                    i2 += 1
+                else:
+                    i1 += 1
+                    i2 += 1
+            else:
+                i1 += 1
+                i2 += 1
+        largest = max(len(parts_1), len(parts_2))
+        return num_wrong / largest, num_wrong, largest
+
+
+# return the date time formatted, e.g. "2017-08-10, 14:34:04"
+def get_time_str():
+    return datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
 
 
 if __name__ == '__main__':
@@ -37,12 +61,15 @@ if __name__ == '__main__':
     base_path = os.path.join(os.path.dirname(__file__), 'data/wer_test')
     test_file_list = [item for item in open(os.path.join(base_path, 'test_data.csv')).read().strip().split('\n')]
     counter = 1
+    total_errors = 0
+    total_words = 0
     for test_file in test_file_list:
         if len(test_file) > 0:
             parts = test_file.split(',')
             if len(parts) == 2:
                 filename = parts[0]
-                print("checking " + filename + " (" + str(counter) + "/" + str(len(test_file_list)) + ")")
+                print("|---------------------------------------------------------------------------- " + get_time_str())
+                print("| checking " + filename + " (" + str(counter) + "/" + str(len(test_file_list)) + ")", end=',')
                 counter += 1
                 text1 = parts[1].strip().lower()
 
@@ -51,15 +78,19 @@ if __name__ == '__main__':
                 text_output_tuple = deep_speech_tt(conf["DeepSpeech"], temp_name, input_sound_file, 60)
                 text2 = ''.join([item for item, _ in text_output_tuple])
 
-                wer = calc_wer(text1, text2)
-                if wer == -1.0:
-                    m = SequenceMatcher(None, text1, text2)
-                    wer = 1.0 - m.ratio()
+                wer, errs, count = calc_wer(text1, text2)
                 wer_list.append(wer)
-                print("         " + filename + " WER:" + str(round(wer, 5)))
-                print(text1)
-                print(text2)
-                print()
+                total_errors += errs
+                total_words += count
 
+                c_wer = total_errors / total_words
+                print(" WER:" + str(round(wer, 5)) + ", cumulative WER:" + str(round(c_wer, 5)))
+                print("| " + text1)
+                print("| " + text2)
+
+    # cumulative word error rate - total number of errors over the total number of words
+    c_wer = total_errors / total_words
     mean = reduce(lambda x, y: x + y, wer_list) / len(wer_list)
-    print("\nmean WER:" + str(round(mean, 5)))
+    print("|---------------------------------------------------------------------------------------------")
+    print("| average WER:" + str(round(mean, 5)) + ", final WER:" + str(round(c_wer, 5)))
+    print("|---------------------------------------------------------------------------------------------")
