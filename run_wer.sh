cc32381614c48436a5443582196b4a064a11305a
Rock de Vocht <peter@peter.co.nz>
2017-08-14 09:09:36 +1200

    fixed run_wer

--- run_wer.sh
+++ run_wer.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-cd /opt/DeepSpeech
-. /opt/DeepSpeech/.env/bin/activate
-python3 ./calculate_test_wer.py >> /opt/DeepSpeech/wer_log.txt
+base=/home/peter/dev/booktrack/DeepSpeech
+cd $base
+. $base/.env/bin/activate
+
+while [ 1 ]
+do
+
+  # rebuild using current graph
+  graph=$base/data/graph
+  if [ -e $graph/input_graph.pb ];
+  then
+    rm -f $graph/input_graph.pb
+    rm -f $graph/output_graph.pb
+    rm -rf $graph/00000001
+  fi
+
+  # create new graph
+  python3 ./export_graph.py --checkpoint_dir $base/data/ckpt --export_dir $graph
+
+  # run wer
+  python3 ./calculate_test_wer.py >> $base/wer_log.txt
+
+  sleep 1h
+
+done
